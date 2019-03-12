01a04c83c6c0ee15872159456a41290de0142566
Andreas Trütschel <andreas@truetschel.net>
2019-03-12 18:29:00 +0100

    Repaired routing sample

--- samples/Routing.DefaultRouterSample/Routing.DefaultRouterSample.Server/Program.cs
+++ samples/Routing.DefaultRouterSample/Routing.DefaultRouterSample.Server/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Routing.DefaultRouterSample.Server
+namespace Routing.DefaultRouterSample
 {
     public class Program
     {
--- samples/Routing.DefaultRouterSample/Routing.DefaultRouterSample.Server/Startup.cs
+++ samples/Routing.DefaultRouterSample/Routing.DefaultRouterSample.Server/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Routing.DefaultRouterSample.Server
+namespace Routing.DefaultRouterSample
 {
     public class Startup
     {
@@ -15,19 +11,35 @@ namespace Routing.DefaultRouterSample.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorComponents<App.Startup>();
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
+            services.AddRazorComponents();
+
+            new App.Startup().ConfigureServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseRazorComponents<App.Startup>();
+
+            app.UseRouting(routes =>
+            {
+                routes.MapRazorPages();
+                routes.MapComponentHub<App.App>("app");
+            });
         }
     }
 }
